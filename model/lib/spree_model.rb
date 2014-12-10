91ae53ca7a1b2ef6e4612d49cfcbfaf9009ee658
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-12-10 03:47:23 -0500

    Dry up spree_model.

--- model/lib/spree_model.rb
+++ model/lib/spree_model.rb
@@ -1,28 +1,14 @@
-require 'virtus'
-require 'inflecto'
 require 'active_model'
-
-# require models
-require 'models/spree/address'
-require 'models/spree/cart'
-require 'models/spree/order'
-require 'models/spree/variant'
-require 'models/spree/product'
-require 'models/spree/promotion'
-require 'models/spree/payment'
-require 'models/spree/customer'
-require 'models/spree/item'
-require 'models/spree/credit'
-require 'models/spree/shipping_method'
-require 'models/spree/shipping_rate'
-require 'models/spree/shipment'
-require 'models/spree/refund'
-
-Virtus.finalize
+require 'inflecto'
+require 'virtus'
 
 # require exceptions
-require 'exceptions/spree/attribute_locked'
-require 'exceptions/spree/illegal_operation'
+Dir["./lib/exceptions/**/*.rb"].sort.each { |f| require f }
+
+# require models
+Dir["./lib/models/**/*.rb"].sort.each { |f| require f }
+
+Virtus.finalize
 
 # Remove annoying deprecation message
 I18n.enforce_available_locales = false
