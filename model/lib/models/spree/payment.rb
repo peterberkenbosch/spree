e0a4a3c7cf798cbab39d0d17fef3f352ff02b2e5
Sean Schofield <sean@spreecommerce.com>
2014-12-10 15:29:08 -0500

    Add new states for payments

--- model/lib/models/spree/payment.rb
+++ model/lib/models/spree/payment.rb
@@ -2,17 +2,43 @@ module Spree
   class Payment
     include Virtus.model(finalize: false)
 
-    attribute :final, Boolean
     attribute :canceled, Boolean, :default => false
+    attribute :refunded, Boolean, :default => false
+    attribute :paid, Boolean, :default => false
+    attribute :partially_refunded, Boolean, :default => false
+    attribute :customer, 'Spree::PaymentMethod'
     attribute :amount, Float
 
     def cancel!
+      raise Spree::IllegalOperation.new('Cannot cancel a refunded payment') if self.refunded
+      raise Spree::IllegalOperation.new('Cannot cancel a canceled payment') if self.canceled
+      raise Spree::IllegalOperation.new('Cannot cancel a paid payment') if self.paid
+      raise Spree::IllegalOperation.new('Cannot cancel a a partially refunded payment') if self.partially_refunded
+
       self.canceled = true
       # TODO - potentially do something with the payment method, etc. (like void the payment on gateway)
     end
 
     def canceled?
       self.canceled
+    end
+
+    def refund!(amount=nil)
+      raise Spree::IllegalOperation.new('Cannot refund a canceled payment') if self.canceled
+      raise Spree::IllegalOperation.new('Cannot refund a canceled payment') if self.refunded
+
+    end
+
+    def refunded?
+      self.refunded
+    end
+
+    def pay!
+      raise Spree::IllegalOperation.new('Cannot pay a refunded payment') if self.refunded
+      raise Spree::IllegalOperation.new('Cannot pay a canceled payment') if self.canceled
+      raise Spree::IllegalOperation.new('Cannot pay a paid payment') if self.paid
+      raise Spree::IllegalOperation.new('Cannot pay a partially refunded payment') if self.partially_refunded
+      self.paid = true
     end
 
     def amount=(value)
--- model/spec/models/spree/payment_spec.rb
+++ model/spec/models/spree/payment_spec.rb
@@ -4,11 +4,114 @@ describe Spree::Payment do
   let(:payment) { Spree::Payment.new }
 
   describe '#cancel!' do
+    context 'when unpaid' do
+      subject(:payment) { Spree::Payment.new(:paid => false) }
+      before { payment.cancel! }
 
-    before { payment.cancel! }
+      it 'changes the state to canceled' do
+        expect(payment.canceled?).to eq(true)
+      end
+    end
 
-    it 'changes the state to canceled' do
-      expect(payment.canceled?).to eq(true)
+    context 'when paid' do
+      before { payment.paid = true }
+
+      it 'raises an exception' do
+        expect{ payment.cancel! }.to raise_exception(Spree::IllegalOperation)
+      end
+    end
+
+    context 'when refunded' do
+      before { payment.refunded = true }
+
+      it 'raises an exception' do
+        expect{ payment.cancel! }.to raise_exception(Spree::IllegalOperation)
+      end
+    end
+
+    context 'when partially refunded' do
+      before { payment.partially_refunded = true }
+
+      it 'raises an exception' do
+        expect{ payment.cancel! }.to raise_exception(Spree::IllegalOperation)
+      end
+    end
+
+  end
+
+  describe '#refund!' do
+    context 'when paid' do
+      before { payment.paid = true }
+
+      it 'does stuff'
+    end
+
+    context 'when partially refunded' do
+      before { payment.partially_refunded = true }
+
+      it 'does stuff'
+    end
+
+    context 'when canceled' do
+      before { payment.canceled = true}
+
+      it 'raises an exception' do
+        expect{ payment.refund! }.to raise_exception(Spree::IllegalOperation)
+      end
+    end
+
+    context 'when refunded' do
+      before { payment.refunded = true}
+
+      it 'raises an exception' do
+        expect{ payment.refund! }.to raise_exception(Spree::IllegalOperation)
+      end
+    end
+  end
+
+  describe '#pay!' do
+    context 'when unpaid' do
+      let(:payment_method) { Spree::PaymentMethod.new }
+      subject(:payment) { Spree::Payment.new(paid: false, payment_method: payment_method) }
+      before { payment.pay! }
+
+      it 'sets the state to paid' do
+        expect(payment.paid).to eq(true)
+      end
+
+      it 'payment method captures the payment'
+    end
+
+    context 'when paid' do
+      before { payment.paid = true }
+
+      it 'raises an exception' do
+        expect{ payment.pay! }.to raise_exception(Spree::IllegalOperation)
+      end
+    end
+
+    context 'when canceled' do
+      before { payment.canceled = true}
+
+      it 'raises an exception' do
+        expect{ payment.pay! }.to raise_exception(Spree::IllegalOperation)
+      end
+    end
+
+    context 'when refunded' do
+      before { payment.refunded = true}
+
+      it 'raises an exception' do
+        expect{ payment.pay! }.to raise_exception(Spree::IllegalOperation)
+      end
+    end
+
+    context 'when partially refunded' do
+      before { payment.partially_refunded = true}
+
+      it 'raises an exception' do
+        expect{ payment.pay! }.to raise_exception(Spree::IllegalOperation)
+      end
     end
   end
 
